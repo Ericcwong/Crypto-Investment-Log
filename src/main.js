3cf6c06c80c467cfa6c0ec7c5ef25f9725f6ea67
Eric Wong <ericwong12@live.com>
2021-11-30 16:42:55 -0700

    Install dev dependencies

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { store } from "./store/cryptos.js";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import "./assets/styles/style.css";
 
-createApp(App).mount('#app')
+library.add(faSearch);
+createApp(App)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .use(store)
+  .mount("#app");
