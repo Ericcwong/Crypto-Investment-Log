41dff9f21a95c7f20bb149b0617fa5caba6432c6
Eric Wong <ericwong12@live.com>
2021-12-03 16:55:35 -0700

    Feat: CurrentInvestmnet
    Display user's investment.
    Create new investments.

---
+++ src/components/UserInvestment/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex justify-evenly">
+    <p>Current investments</p>
+    <!-- New Investment Modal -->
+    <button
+      class="text-white rounded bg-gray-500 px-3"
+      @click.prevent="newInvestment = !newInvestment"
+    >
+      New Investment
+    </button>
+    <NewInvestment v-if="newInvestment" />
+  </div>
+  <Table />
+</template>
+
+<script setup>
+import { ref } from "vue";
+import NewInvestment from "./NewInvestment.vue";
+import Table from "./Table.vue";
+const newInvestment = ref(false);
+</script>
+
+<style></style>
