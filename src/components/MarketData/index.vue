9b40f25fe307399c0a3d532f4e17330bb13dfda4
Eric Wong <ericwong12@live.com>
2021-12-03 16:54:50 -0700

    Feat: Market-Table
    Market table is to display current market data.

---
+++ src/components/MarketData/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="market-table">
+    <div class="border-white">BTC $54000</div>
+    <div class="border-gray-50">ETH $4200</div>
+    <div class="test-data">BNB $700</div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style></style>
