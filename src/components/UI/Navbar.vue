ea98210475e3a0644ef8758861ae5d0b14da6a97
Eric Wong <ericwong12@live.com>
2021-12-06 16:41:23 -0700

    Feat: Navbar.vue
    Add Navbar to application.

---
+++ src/components/UI/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="flex justify-between pt-4">
+    <div class="header">Investment Log</div>
+    <div class="user">
+      <button
+        v-if="!isLogin"
+        class="text-white hover:bg-gray-500 rounded"
+        @click="signIn"
+      >
+        Sign in
+      </button>
+      <div class="sign-in flex" v-else>
+        <div class="avatar mr-4">
+          <img class="rounded-full w-8" :src="user.photoURL" alt="" />
+        </div>
+        <button class="text-white hover:bg-gray-900" @click="signOut">
+          Sign out
+        </button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { useAuth } from "../../firebase";
+const { user, isLogin, signOut, signIn } = useAuth();
+</script>
+
+<style></style>
