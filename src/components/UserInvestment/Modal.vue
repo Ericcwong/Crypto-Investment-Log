1b357a13e6e75f5a4d8e3c26e15222a98a416be1
Eric Wong <ericwong12@live.com>
2021-12-06 16:42:35 -0700

    Feat-inprogress: index.vue (New investment)
    Working towards a modal popup when user clicks new investment.

--- src/components/UserInvestment/Modal.vue
+++ src/components/UserInvestment/Modal.vue
@@ -1,12 +1,34 @@
 <template>
   <div class="flex justify-evenly">
     <p>Current investments</p>
+    <!-- New Investment Modal -->
     <button
       class="text-white rounded bg-gray-500 px-3"
       @click.prevent="openModal()"
     >
       New Investment
     </button>
+    <Teleport to="body">
+      <!-- Background when modal is clicked -->
+      <div
+        class="
+          modal-background
+          fixed
+          bg-gray-500 bg-opacity-50
+          w-full
+          h-screen
+          inset-0
+        "
+      >
+        <div class="flex justify-center items-center pt-24">
+          <div class="modal bg-white shadow-xl w-1/2 p-4">
+            <NewInvestment v-if="newInvestment" />
+          </div>
+        </div>
+      </div>
+
+      <Table />
+    </Teleport>
   </div>
 </template>
 
