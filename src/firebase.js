a1e670b61fb5d038980190a4e652afe6a933f809
Eric Wong <ericwong12@live.com>
2021-12-07 16:46:57 -0700

    Fix: firebase.js
    Firebase is updated to use modules.
    All user sign in and sign out is handled in Vuex.

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,16 @@
 // Documentation: https://firebase.google.com/docs/web/modular-upgrade
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
-import { reactive, onMounted, computed } from "vue";
-import { store } from "./store/index";
-// const store = useStore();
-console.log(store.state.user.user);
-firebase.initializeApp({
+// Modular components help with smaller file sizes. :D
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
+// Initialize Firebase with config information.
+initializeApp({
   apiKey: "AIzaSyB1L8sGDtfCIyJdYirmgiOIG2S-y_zvDc4",
   authDomain: "crypto-investment-log.firebaseapp.com",
   projectId: "crypto-investment-log",
@@ -15,26 +19,8 @@ firebase.initializeApp({
   appId: "1:607867618695:web:7479c05311a574a5503f22",
   measurementId: "G-5BH9QR1XYE",
 });
-const db = firebase.firestore();
-const auth = firebase.auth();
 
-export function useAuth() {
-  const user = reactive({
-    data: null,
-  });
-  const unsubscribe = auth.onAuthStateChanged((_user) => (user.data = _user));
-  onMounted(() => {
-    unsubscribe;
-  });
-  const isLogin = computed(() => user.data !== null);
-  const signIn = async () => {
-    const googleProvider = new firebase.auth.GoogleAuthProvider();
-    await auth.signInWithPopup(googleProvider);
-    store.commit("user/getUserData", user.data);
-  };
-  const signOut = () => {
-    store.commit("user/clearUser");
-    auth.signOut();
-  };
-  return { user, isLogin, signIn, signOut };
-}
+const auth = getAuth();
+const db = getFirestore();
+const provider = new GoogleAuthProvider();
+export { auth, db, provider, signInWithPopup, GoogleAuthProvider };
