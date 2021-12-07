bfda0d9d9c2b804b18e5fc60349e8309994be0c3
Eric Wong <ericwong12@live.com>
2021-12-07 16:50:05 -0700

    Fix: user.js
    Switch to firebase modules.

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,13 @@
-const state = () => ({
+import {
+  auth,
+  provider,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "@/firebase.js";
+
+const state = {
   user: null,
-});
+};
 
 const mutations = {
   getUserData(state, payload) {
@@ -15,7 +22,29 @@ const mutations = {
   },
 };
 
-const actions = {};
+const actions = {
+  signIn() {
+    signInWithPopup(auth, provider)
+      .then((results) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  },
+};
 
 const getters = {};
 
