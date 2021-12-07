5a801961de034f67588db54fcdf45d23dbe173bb
Eric Wong <ericwong12@live.com>
2021-12-07 02:54:21 -0700

    Feat: user.js (Store)
    mutation to save and clear data to state.

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,14 +1,26 @@
 const state = () => ({
-  user: [],
+  user: null,
 });
 
-const mutations = {};
+const mutations = {
+  getUserData(state, payload) {
+    console.log("Called mutation");
+    console.log(payload);
+    state.user = payload;
+    console.log(state.user);
+  },
+  clearUser(state) {
+    state.user = null;
+    console.log(state.user);
+  },
+};
 
 const actions = {};
 
 const getters = {};
 
 export default {
+  namespaced: true,
   state,
   mutations,
   actions,
