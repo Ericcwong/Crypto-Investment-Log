d38b43b0a892737a07bed044c7a1461e9b444d0e
Eric Wong <ericwong12@live.com>
2021-12-09 16:35:43 -0700

    Fix: store/user.js
    Removed sign in and out function to firebase file.

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,50 +1,25 @@
-import {
-  auth,
-  provider,
-  signInWithPopup,
-  GoogleAuthProvider,
-} from "@/firebase.js";
-
 const state = {
   user: null,
+  userErrorCode: "",
+  userErrorMessage: "",
 };
 
 const mutations = {
   getUserData(state, payload) {
-    console.log("Called mutation");
-    console.log(payload);
     state.user = payload;
-    console.log(state.user);
+  },
+  loginErrorCode(state, { errorCode, errorMessage }) {
+    state.userErrorCode = errorCode;
+    state.userErrorMessage = errorMessage;
   },
   clearUser(state) {
     state.user = null;
-    console.log(state.user);
+    state.userErrorCode = "";
+    state.userErrorMessage = "";
   },
 };
 
-const actions = {
-  signIn() {
-    signInWithPopup(auth, provider)
-      .then((results) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-        // The signed-in user info.
-        const user = result.user;
-        // ...
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.email;
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
-      });
-  },
-};
+const actions = {};
 
 const getters = {};
 
