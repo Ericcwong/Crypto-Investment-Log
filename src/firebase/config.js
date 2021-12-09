68dcc6e389e8d90a315d6e27578e1a155668648e
Eric Wong <ericwong12@live.com>
2021-12-09 16:33:15 -0700

    Chore: firebase/config.js
    A file dedicated to firebase configuration.

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+// Documentation: https://firebase.google.com/docs/web/modular-upgrade
+// Modular components help with smaller file sizes. :D
+import { initializeApp } from "firebase/app";
+
+// Initialize Firebase with config information.
+export const firebaseApp = initializeApp({
+  apiKey: "AIzaSyB1L8sGDtfCIyJdYirmgiOIG2S-y_zvDc4",
+  authDomain: "crypto-investment-log.firebaseapp.com",
+  projectId: "crypto-investment-log",
+  storageBucket: "crypto-investment-log.appspot.com",
+  messagingSenderId: "607867618695",
+  appId: "1:607867618695:web:7479c05311a574a5503f22",
+  measurementId: "G-5BH9QR1XYE",
+});
