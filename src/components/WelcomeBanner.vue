87a701bcf8131288ee32b1f3aad2e5761b04837b
Eric Wong <ericwong12@live.com>
2021-12-09 21:08:12 -0700

    Feat: WelcomeBanner
    Shows user's portfolio status.

---
+++ src/components/WelcomeBanner.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="text-center" v-if="!isLogin">Loading data...</h1>
+  <h1 class="pt-8 text-center" v-else>
+    Welcome {{ user.displayName.split(" ")[0] }}, Portfolio is at (price) (up or
+    down percentage)
+  </h1>
+</template>
+
+<script setup>
+import { useAuth } from "@/firebase/user";
+const { isLogin, user } = useAuth();
+</script>
+
+<style></style>
