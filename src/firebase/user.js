b94f7d9777c273d37bea5bed4dfdf30521d25eee
Eric Wong <ericwong12@live.com>
2021-12-09 21:09:09 -0700

    Chore: firebase/user.js
    Clean up code to use try catch.
    Remove unused code.

--- src/firebase/user.js
+++ src/firebase/user.js
@@ -9,7 +9,7 @@ import {
 // Store Import
 import store from "@/store/index.js";
 // Vue functions
-import { computed, onMounted, onUnmounted, ref } from "vue";
+import { computed, onUnmounted, ref } from "vue";
 
 // Calling auth function from firebase and using the configuration.
 const auth = getAuth(firebaseApp);
@@ -18,6 +18,10 @@ const auth = getAuth(firebaseApp);
 // User's sign in data is still there until they sign out.
 export const useAuth = () => {
   const user = ref(null);
+  const error = {
+    code: null,
+    message: null,
+  };
   const unsubscribe = auth.onAuthStateChanged(
     (userData) => (user.value = userData)
   );
@@ -26,27 +30,17 @@ export const useAuth = () => {
   const isLogin = computed(() => user.value !== null);
   const provider = new GoogleAuthProvider();
   const googleSignIn = () => {
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-        // The signed-in user info.
-        const user = result.user;
-        // Pass user information to state
-        store.commit("user/getUserData", user);
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.email;
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        // Pass error information to state
-        // store.commit("user/loginErrorCode", { errorCode, errorMessage });
-      });
+    try {
+      signInWithPopup(auth, provider);
+    } catch (error) {
+      // Handle Errors here.
+      error.code = err.code;
+      error.message = err.message;
+      // The email of the user's account used.
+      const email = err.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(err);
+    }
   };
 
   const googleSignOut = () => {
