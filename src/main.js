1777df7bf66a9f8a2944a83910257b8fcf71193d
Eric Wong <ericwong12@live.com>
2021-12-14 08:17:13 -0700

    Feat: main.js
    Vue router access throughout application.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store/index.js";
+import router from "./router";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import "./styles/style.css";
 
 library.add(faSearch);
+
 createApp(App)
   .component("font-awesome-icon", FontAwesomeIcon)
+  .use(router)
   .use(store)
   .mount("#app");
