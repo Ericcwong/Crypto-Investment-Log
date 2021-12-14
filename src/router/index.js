698810195cd6849beb7e1d31872725e416ca8a55
Eric Wong <ericwong12@live.com>
2021-12-14 08:18:54 -0700

    Fix: router
    Add component to render home page.

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/pages/home.vue";
 const routes = [
   {
     path: "/",
     name: "Home",
+    component: Home,
   },
 ];
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(),
   routes,
 });
 
