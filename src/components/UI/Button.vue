0dd8f7052a3bdd1648cf1241d3e0ca0b0a7002a2
Eric Wong <ericwong12@live.com>
2022-01-17 15:52:29 -0700

    Feat: Button
    Universal button to handle navigation

---
+++ src/components/UI/Button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button class="text-white rounded bg-gray-500 px-3" @click="runFunction">
+    <font-awesome-icon
+      v-if="Icon !== undefined"
+      :icon="Icon"
+    ></font-awesome-icon>
+    {{ name }}
+  </button>
+</template>
+
+<script setup>
+/* Button component requires the parent component to have @runFunction following
+ ** the function name. Example: Parent: @runFunction = "toggleModal"
+ */
+const props = defineProps({
+  name: String,
+  Icon: String,
+});
+const emit = defineEmits(["runFunction"]);
+const runFunction = (data) => {
+  emit("runFunction");
+};
+</script>
+
+<style scoped></style>
