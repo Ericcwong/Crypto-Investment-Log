9e1bd4f44c7fd63618f500a4de90ac5070121850
Eric Wong <ericwong12@live.com>
2022-01-16 18:17:10 -0700

    Feat: UserInvestment
    Modal add

--- src/components/UserInvestment/index.vue
+++ src/components/UserInvestment/index.vue
@@ -1,27 +1,21 @@
 <template>
   <div class="flex justify-evenly">
     <p>Current investments</p>
-    <button
-      class="text-white rounded bg-gray-500 px-3"
-      @click.prevent="openModal()"
-    >
-      New Investment
-    </button>
+    <button class="text-white rounded bg-gray-500 px-3">New Investment</button>
+    <Modal @close="toggleModal" :modalActive="modalActive">
+      <NewInvestment />
+    </Modal>
+    <button @click="toggleModal" v-if="!modalActive">Open Modal</button>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import Modal from "@/components/UI/Modal/index.vue";
 import NewInvestment from "./NewInvestment.vue";
-import Table from "./Table.vue";
-
-const newInvestment = ref(true);
-const openModal = (event) => {
-  console.log("Clicked button");
-  const overlay = document.getElementById("overlay");
-  overlay.classList.remove("hidden");
-  overlay.classList.add("flex");
-  newInvestment.value = !newInvestment.value;
+const modalActive = ref(false);
+const toggleModal = () => {
+  modalActive.value = !modalActive.value;
 };
 </script>
 
