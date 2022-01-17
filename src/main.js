ab95becb056585c9cd3c02267211a9ace36b18ea
Eric Wong <ericwong12@live.com>
2022-01-16 18:15:47 -0700

    Style: Main.js
    Add (x) close icon

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import App from "./App.vue";
 import store from "./store/index.js";
 import router from "./router";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faSearch, faTimes } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSearch,
+  faTimes,
+  faTimesCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import "./styles/style.css";
 
-library.add(faSearch, faTimes);
+library.add(faSearch, faTimes, faTimesCircle);
 
 createApp(App)
   .component("font-awesome-icon", FontAwesomeIcon)
