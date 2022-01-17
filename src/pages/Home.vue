eda4d50247a50a7fb31e2626bdf1a4680840d01d
Eric Wong <ericwong12@live.com>
2022-01-16 18:18:08 -0700

    Style: Home (Page)
    Removed all style to prep for tailwind.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,7 @@
 <template>
   <div class="page-container">
-    <section class="welcome-banner h-16" v-if="isLogin">
-      <WelcomeBanner />
-    </section>
     <section id="current-investment" class="investment-body">
       <UserInvestment />
-    </section>
-    <section class="market-data">
-      Market data
-      <MarketData />
     </section>
   </div>
 </template>
@@ -21,36 +14,4 @@ import { useAuth } from "@/firebase/user";
 const { isLogin, user } = useAuth();
 </script>
 
-<style scoped>
-#app {
-  /* background-color: #1f2033; */
-  color: black;
-  height: 100vh;
-  padding-left: 2rem;
-  padding-right: 2rem;
-}
-
-.page-container {
-  display: grid;
-  gap: 1rem;
-  grid-template-columns: 15% 85%;
-  grid-template-areas:
-    "navbar welcomeBanner"
-    "investmentBody investmentBody"
-    "marketData marketData";
-  height: 100vh;
-}
-
-.navbar {
-  grid-area: navbar;
-}
-.welcome-banner {
-  grid-area: welcomeBanner;
-}
-.investment-body {
-  grid-area: investmentBody;
-}
-.market-data {
-  grid-area: marketData;
-}
-</style>
+<style scoped></style>
