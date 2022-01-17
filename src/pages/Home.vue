e3bfc3ca9c179bcaa771504045f74f443f986e5a
Eric Wong <ericwong12@live.com>
2022-01-17 15:54:35 -0700

    Fix: Home.vue
    Home page content is empty for now. Prep to show user data

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,11 @@
 <template>
-  <div class="page-container">
-    <section id="current-investment" class="investment-body">
-      <UserInvestment />
-    </section>
+  <div>
+    <section id="current-investment" class="investment-body"></section>
   </div>
 </template>
 
 <script setup>
 import WelcomeBanner from "@/components/UI/HomePage/WelcomeBanner.vue";
-import UserInvestment from "@/components/UserInvestment/index.vue";
 import MarketData from "@/components/MarketData/index.vue";
 import { useAuth } from "@/firebase/user";
 const { isLogin, user } = useAuth();
