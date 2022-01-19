f7945db90aaddc81b334e271c35af8aab8435899
Eric Wong <ericwong12@live.com>
2022-01-19 00:31:14 -0700

    Fix: NewInvestment.vue
    Broke file down to smaller components.
    Could clean up some more.

--- src/components/UserInvestment/NewInvestment.vue
+++ src/components/UserInvestment/NewInvestment.vue
@@ -1,85 +1,58 @@
 <template>
+  <!-- Wait for User to input a crypto the want -->
   <form>
-    <div class="input-container">
+    <div class="flex justify-between items-center border w-2/4">
+      <span class="z-10 absolute">
+        <font-awesome-icon class="icon ml-1" icon="search" />
+      </span>
       <input
         type="text"
         placeholder="Name"
         v-model="investment.cryptoName"
         @input="filterData"
-        class="search-box"
+        class="w-full outline-none pl-6 relative"
       />
-      <font-awesome-icon class="icon" icon="search" />
-      <div
-        class="search-box"
-        v-if="investment.matches !== null"
-        v-for="match in investment.matches"
-        :key="match.id"
-      >
-        <div
-          @click.prevent="getCurrentPrice(match.current_price)"
-          class="search-results"
-        >
-          <img class="search-image" :src="match.image" alt="" />
-          <div class="name">
-            <p>{{ match.name }}</p>
-            <span>({{ match.symbol }})</span>
-          </div>
-          <span>#{{ match.market_cap_rank }}</span>
-        </div>
-      </div>
-    </div>
-    <input
-      type="number"
-      placeholder="Share Purchase price"
-      v-model="investment.purchasePrice"
-      @input="calcTotal"
-    />
-    <input
-      type="number"
-      placeholder="Amount of Shares"
-      v-model="investment.shareAmount"
-      @input="calcTotal"
-    />
-    <p>Total Amount Invested ${{ investment.total }}</p>
-    <div class="options">
-      <button>Short term investment</button>
-      <span>or</span>
-      <button>Long term investment</button>
     </div>
   </form>
-  {{ investment.matches }}
+  <SearchResults
+    @getPrice="getCurrentPrice"
+    :matches="investment.matches"
+    v-if="investment.crypto === null"
+  />
+  <CreateTransaction
+    v-if="investment.crypto !== null"
+    :crypto="investment.crypto"
+  />
 </template>
 
 <script setup>
+import SearchResults from "@/components/UI/NewInvestment/Results.vue";
+import CreateTransaction from "@/components/UI/NewInvestment/CreateTransaction.vue";
 import { onMounted, reactive, computed } from "vue";
 import { useStore } from "vuex";
 import { mapGetters } from "vuex";
+
 const store = useStore();
 const investment = reactive({
   cryptoName: "",
-  purchasePrice: null,
+  crypto: null,
   shareAmount: null,
   total: 0,
   matches: null,
 });
 onMounted(() => {
   retrieveData();
-  //   filterData();
 });
 
 const retrieveData = async () => {
-  // Checks if Vuex state is empty, if so call and update the data
-  if (store.state.cryptos !== []) {
-    console.log("Updating State");
-    store.dispatch("cryptos/loadCryptos");
-  }
+  store.dispatch("cryptos/loadCryptos");
 };
 const filterData = async () => {
   // User inputs will filter through Vuex state to find the crypto by ID or Symbol
   // Stores data retrieved into matches
   let name = investment.cryptoName;
+  investment.crypto = null;
   let getCrypto = await store.getters["cryptos/getCryptoByName"](name);
-  //   console.log(store);
   // Clear Matches after search
   if (name === "") {
     investment.matches = [];
@@ -89,60 +62,10 @@ const filterData = async () => {
   }
 };
 const getCurrentPrice = (data) => {
-  investment.purchasePrice = data;
+  investment.crypto = data;
+  investment.cryptoName = "";
   investment.matches = [];
-};
-const calcTotal = () => {
-  const total = investment.purchasePrice * investment.shareAmount;
-  investment.total = total.toFixed(2);
 };
 </script>
 
-<style scoped>
-/* form {
-  display: grid;
-  padding: 2rem;
-  border: 1px solid black;
-  border-radius: 0.2rem;
-  max-width: 35%;
-  color: white;
-}
-input {
-}
-.input-container {
-  position: relative;
-}
-.input-container input {
-  width: 100%;
-}
-.input-container .icon {
-  position: absolute;
-  top: 10%;
-  right: 0;
-  margin-right: 10px;
-}
-.search-box {
-  margin-bottom: 1.5rem;
-}
-.search-results {
-  display: grid;
-  grid-template-columns: 20% 60% 20%;
-  color: white;
-  margin-bottom: 1rem;
-  align-items: center;
-  text-decoration: none;
-}
-.search-results:hover {
-  cursor: pointer;
-}
-.name {
-  display: flex;
-}
-.search-image {
-  width: 2rem;
-}
-.options {
-  display: flex;
-  justify-content: space-around;
-} */
-</style>
+<style scoped></style>
