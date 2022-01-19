a1c28f511c730be0a2d5a9e192c0d4364c7f12db
Eric Wong <ericwong12@live.com>
2022-01-19 00:30:00 -0700

    Feat: Results.vue
    User search results

---
+++ src/components/UI/NewInvestment/Results.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="h-full overflow-y-auto">
+    <div class="" v-for="match in matches" :key="match.id">
+      <div
+        @click="getMatch(match)"
+        class="flex items-center hover: cursor-pointer"
+      >
+        <img class="w-12" :src="match.image" alt="" />
+        <p>{{ match.name }}</p>
+        <span class="uppercase">({{ match.symbol }})</span>
+        <span>#{{ match.market_cap_rank }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineEmits } from "vue";
+const props = defineProps({
+  matches: Array,
+});
+const emit = defineEmits(["getPrice"]);
+const getMatch = (data) => {
+  console.log(data);
+  emit("getPrice", data);
+};
+</script>
+
+<style scoped></style>
