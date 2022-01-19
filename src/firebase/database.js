0fd466aa0d0e8b325082e7eef8c72a71acf8b90d
Eric Wong <ericwong12@live.com>
2022-01-19 15:57:20 -0700

    Feat: Store
    Add investment boilerplate.

--- src/firebase/database.js
+++ src/firebase/database.js
@@ -4,7 +4,16 @@ import {
   collection,
   query,
   where,
+  addDoc,
   getDocs,
 } from "firebase/firestore";
 
 const db = getFirestore(firebaseApp);
+
+const colRef = collection(db, "investment");
+
+export const addInvestment = (data) => {
+  addDoc(colRef, {
+    data,
+  });
+};
