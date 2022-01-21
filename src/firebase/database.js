81493940197f9acbbf9faecaab676690566f4c81
Eric Wong <ericwong12@live.com>
2022-01-21 16:15:30 -0700

    Feat: database.js
    Creating an investment is tied to user.
    That is sent to a collection that is based in the coin's id.

--- src/firebase/database.js
+++ src/firebase/database.js
@@ -6,19 +6,44 @@ import {
   where,
   addDoc,
   getDocs,
+  setDoc,
+  doc,
 } from "firebase/firestore";
+import { useAuth } from "./user";
+// User needs to have an account to start saving data to their UID
+const { user, isLogin, googleSignIn, googleSignOut } = useAuth();
 // Database base
 const db = getFirestore(firebaseApp);
 // Collection Reference
 const colRef = collection(db, "investment");
+// User's UID
+
+export const loadInvestments = async (data) => {
+  try {
+    const loadDoc = await getDocs(collection(db, "investment"));
+    loadDoc.forEach((doc) => {
+      console.log(doc.data());
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const addInvestment = async (data) => {
   try {
-    // const docRef = await addDoc(colRef, {
-    //   data,
-    // });
-    console.log("Document Data", data);
+    const userData = user;
+    const userUID = await userData._value.uid;
+    console.log(user);
+    console.log("Document Data", data.collection);
+
+    const docRef = await collection(db, "investment", userUID, data.collection);
+    await setDoc(doc(docRef), {
+      data,
+    });
+    console.log(docRef);
   } catch (error) {
-    console.log("Uh oh, there is an error creating the document.");
+    console.log(error);
   }
 };
+
+loadInvestments();
