29acafde1cd4836f9469ca3f0e1ee2f349e55b90
Eric Wong <ericwong12@live.com>
2022-01-23 17:22:51 -0700

    Feat: Home
    Add LoadInvestment component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>
-    <section id="current-investment" class="investment-body"></section>
-  </div>
+  <section id="current-investment" class="investment-body">
+    <LoadInvestment />
+  </section>
 </template>
 
 <script setup>
 import WelcomeBanner from "@/components/UI/HomePage/WelcomeBanner.vue";
 import MarketData from "@/components/MarketData/index.vue";
 import { useAuth } from "@/firebase/user";
-const { isLogin, user } = useAuth();
+import LoadInvestment from "../components/LoadInvestment/index.vue";
 </script>
 
 <style scoped></style>
