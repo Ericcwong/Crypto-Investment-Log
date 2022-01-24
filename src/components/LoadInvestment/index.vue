facef9c3500b3a74856847d3b18016b040c32c34
Eric Wong <ericwong12@live.com>
2022-01-23 20:20:07 -0700

    Feat: LoadInvestment
    layout

--- src/components/LoadInvestment/index.vue
+++ src/components/LoadInvestment/index.vue
@@ -1,19 +1,37 @@
 <template>
-  <!-- <div class="" v-for="crypto in state.userCrypto" :key="crypto.id">
-    <div class="">{{ crypto.collection }}</div>
-  </div> -->
+  <div class="grid gap-8">
+    <div class="" v-for="(cryptos, index) in state.userCrypto" :key="index">
+      <div class="" v-for="(crypto, name) in cryptos" :key="name">
+        <div class="capitalize">{{ name }}</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import { useStore } from "vuex";
-import { reactive, onMounted } from "vue";
+import { reactive, onMounted, computed } from "vue";
 import { loadInvestments } from "@/firebase/database.js";
 onMounted(() => {
   loadInvestments();
 });
 const store = useStore();
 
-// const state = reactive({
-//   userCrypto: store.state.cryptos.userCryptos,
-// });
+console.log(store.state);
+const state = reactive({
+  userCrypto: store.state.cryptos.userCryptos,
+});
+
+const filterData = async () => {
+  let name = investment.cryptoName;
+  investment.crypto = null;
+  let getCrypto = await store.getters["cryptos/getCryptoByName"](name);
+  // Clear Matches after search
+  if (name === "") {
+    investment.matches = [];
+  } else {
+    console.log(getCrypto);
+    investment.matches = getCrypto;
+  }
+};
 </script>
