096e4dd7980fa56f56ce832a3b59794c6d59e7e6
Eric Wong <ericwong12@live.com>
2022-01-23 17:20:54 -0700

    Feat: LoadInvestment
    Component to load user's created investments.

---
+++ src/components/LoadInvestment/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- <div class="" v-for="crypto in state.userCrypto" :key="crypto.id">
+    <div class="">{{ crypto.collection }}</div>
+  </div> -->
+</template>
+
+<script setup>
+import { useStore } from "vuex";
+import { reactive, onMounted } from "vue";
+import { loadInvestments } from "@/firebase/database.js";
+onMounted(() => {
+  loadInvestments();
+});
+const store = useStore();
+
+// const state = reactive({
+//   userCrypto: store.state.cryptos.userCryptos,
+// });
+</script>
