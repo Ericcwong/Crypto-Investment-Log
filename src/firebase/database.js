63953c0deb7ac3cac41fdd79622b16a651e10331
Eric Wong <ericwong12@live.com>
2022-01-26 16:18:47 -0700

    Feat: database.js
    Load data on change.
    Add investment modeling change.

--- src/firebase/database.js
+++ src/firebase/database.js
@@ -10,6 +10,8 @@ import {
   arrayUnion,
   doc,
   updateDoc,
+  onSnapshot,
+  QuerySnapshot,
 } from "firebase/firestore";
 // User needs to have an account to start saving data to their UID
 import store from "@/store/index.js";
@@ -22,51 +24,27 @@ const investmentRef = collection(db, "investment");
 
 export const loadInvestments = async () => {
   try {
-    const userID = await store.state.user.user.uid;
-    console.log(userID);
-    // Query search document by userID
-    const q = query(investmentRef, where("userID", "==", userID));
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, " => ", doc.data());
-    });
+    const userState = store.state.user.user;
+    // Checks if there is a user, if so pull their data.
+    if (userState !== null) {
+      const userID = await store.state.user.user.uid;
+      // Query search document by userID
+      const q = query(investmentRef, where("userID", "==", userID));
+      // When unsubscribe is called, it will stop listening to updates to the database.
+      // Currently it is always listening to changes to investment collection where a document's userID is signed in user's id.
+      const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        const cryptos = [];
+        querySnapshot.forEach((doc) => {
+          cryptos.push(doc.data());
+        });
+        store.dispatch("cryptos/loadUserCryptos", cryptos);
+      });
+    } else {
+      return;
+    }
   } catch (error) {
     console.log("Loading investment: ", error);
   }
-  // try {
-  //   const subCol = [];
-  //   // Target top level collection
-  //   const investmentRef = collection(db, "investment");
-  //   // Target investment collection, find document that is user's id
-  //   const q = query(investmentRef, where("user_id", "==", userID));
-  //   /* Get Documents, documents house save crypto_data.
-  //    * crypto_data is an array that houses all the crypto names the user's have created in new investment.
-  //    */
-  //   const querySnapshot = await getDocs(q);
-  //   // Extracts all the data saved in crypto_data and pushes it to subCol
-  //   querySnapshot.forEach((doc) => {
-  //     doc.data().crypto_data.forEach((getSubCol) => {
-  //       subCol.push(getSubCol);
-  //     });
-  //   });
-  //   // Retrieve all the documents that save into each sub collection.
-  //   subCol.forEach(async (element) => {
-  //     // base object
-  //     let crypto = {};
-  //     // Creates an object with element name and creates an empty array to store document data.
-  //     crypto[element] = new Array();
-  //     const loadDoc = await getDocs(
-  //       collection(db, "investment", userID, element)
-  //     );
-  //     loadDoc.forEach((doc) => {
-  //       crypto[element].push(doc.data());
-  //     });
-  //     store.dispatch("cryptos/loadUserCryptos", crypto);
-  //   });
-  // } catch (error) {
-  //   console.log(error);
-  // }
 };
 
 export const addInvestment = async (data) => {
@@ -89,33 +67,13 @@ export const addInvestment = async (data) => {
   } catch (error) {
     console.log("Add Investment: ", error);
   }
-  // try {
-  //   // console.log("Document Data", data.collection);
-  //   const dataCollection = data.collection;
-  //   // docRef writes to the document of the user. Saving which coin they added to the database
-  //   const docRef = await doc(db, "investment", userID);
-  //   await updateDoc(docRef, {
-  //     crypto_data: arrayUnion(dataCollection),
-  //   });
-  //   // Saves data to sub collections
-  //   const subinvestmentRef = await collection(
-  //     db,
-  //     "investment",
-  //     userID,
-  //     data.collection
-  //   );
-  //   await setDoc(doc(subinvestmentRef), {
-  //     data,
-  //   });
-  // } catch (error) {
-  //   console.log(error);
-  // }
 };
 const addNewUser = async () => {
   const userID = await store.state.user.user.uid;
+
   const username = await store.state.user.user.displayName;
   const userRef = doc(db, "user", userID);
-  await setDoc(doc(userRef), {
+  await setDoc(userRef, {
     username: username,
   });
 };
