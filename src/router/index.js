12119b10baedd90b9fbf16d1fb2eecaf051b524c
Eric Wong <ericwong12@live.com>
2022-01-31 16:42:17 -0700

    Feat: Router.js
    Dynamic route

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/pages/Home.vue";
+import CryptoDetail from "@/pages/user/CryptoDetail.vue";
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/user/:id",
+    name: "CryptoDetail",
+    component: CryptoDetail,
   },
 ];
 const router = createRouter({
