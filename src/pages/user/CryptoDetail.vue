cadf9278e232f32b42d5f2ca73eaa741eba33f4b
Eric Wong <ericwong12@live.com>
2022-01-31 16:42:03 -0700

    Feat: CryptoDetail.vue
    Specific crypto viewing.
    User's investment log

---
+++ src/pages/user/CryptoDetail.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="" v-if="state.data !== null">
+    <div class="header flex items-center">
+      <h1 class="text-3xl capitalize">{{ state.name }}</h1>
+      <img class="w-20" :src="state.data.icon" alt="" />
+    </div>
+    <div class="" v-for="(transaction, index) in state.data.data" :key="index">
+      {{ transaction }}
+    </div>
+  </div>
+  <!-- {{ state.data }} -->
+</template>
+
+<script setup>
+import { useRoute } from "vue-router";
+import { reactive, onMounted, watch } from "vue";
+import { useStore } from "vuex";
+const store = useStore();
+const state = reactive({
+  name: "",
+  data: null,
+});
+onMounted(() => {
+  const route = useRoute();
+  console.log(route.params);
+  state.name = route.params.id;
+  getCrypto();
+});
+watch(
+  () => store.state.cryptos.userCryptos,
+  (first, second) => {
+    state.userCryptos = first;
+    getCrypto();
+  }
+);
+const getCrypto = async () => {
+  const storeState = await store.state.cryptos.userCryptos;
+  console.log(storeState);
+  let data = storeState.find((crypto) => (crypto.collection = state.name));
+  console.log(data);
+  state.data = { ...data };
+};
+</script>
+
+<style scoped></style>
