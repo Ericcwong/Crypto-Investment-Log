19149b008397f737f4606900b9724af38ee4bd75
Eric Wong <ericwong12@live.com>
2022-02-01 01:39:35 -0700

    Style: Home.vue
    Layout change

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,16 +1,20 @@
 <template>
-  <section id="current-investment" class="grid grid-cols-2">
-    <LoadInvestment />
-
-    <div class="">
-      <h1>Market Data</h1>
+  <section
+    id="current-investment"
+    class="grid grid-flow-col grid-cols-2 grid-rows-6 h-screen"
+  >
+    <div class="flex justify-center items-center col-span-2 row-span-4">
+      <Swap />
     </div>
+
+    <div class="col-span-2 row-span-2 bg-assets-bg"><LoadInvestment /></div>
   </section>
 </template>
 
 <script setup>
 import MarketData from "@/components/MarketData/index.vue";
 import LoadInvestment from "../components/LoadInvestment/index.vue";
+import Swap from "@/components/Swap/index.vue";
 </script>
 
 <style scoped>
