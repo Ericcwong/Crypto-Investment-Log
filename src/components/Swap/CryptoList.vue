f81085581ced5a343487a1ac09a4f5ea35fb2028
Eric Wong <ericwong12@live.com>
2022-02-10 16:36:47 -0700

    Feat: CryptoList.vue
    Crypto list is when a user clicks on swap asset and displays all the crypto they have.
    
    Need a search feature for ease of use.

---
+++ src/components/Swap/CryptoList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="text-black">
+    <div
+      class="grid grid-cols-3 items-center"
+      v-for="userCrypto in userCryptos"
+    >
+      <!-- <div class="flex items-center"> -->
+      <img
+        class="w-14"
+        :src="userCrypto.icon"
+        :alt="`${userCrypto.collection} icon`"
+      />
+      <span class="flex justify-center capitalize">{{
+        userCrypto.collection
+      }}</span>
+      <!-- </div> -->
+
+      <span class="flex justify-end">{{ userCrypto.total_quantity }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useStore } from "vuex";
+import { computed } from "vue";
+const store = useStore();
+
+let userCryptos = computed(() => store.getters["cryptos/getUserCrypto"]);
+// console.log(userCryptos);
+</script>
+
+<style scoped></style>
