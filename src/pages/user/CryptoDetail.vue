ecbe4419a4617e1d7850c0933dc8d3bc60842ade
Eric Wong <ericwong12@live.com>
2022-02-10 16:44:06 -0700

    Feat: CryptoDetail.vue
    Add delete investment (document) by id.

--- src/pages/user/CryptoDetail.vue
+++ src/pages/user/CryptoDetail.vue
@@ -3,7 +3,10 @@
     <div class="header flex items-center">
       <h1 class="text-3xl capitalize">{{ state.name }}</h1>
       <img class="w-20" :src="state.data.icon" alt="" />
+      <button @click="deleteDocument">Delete Document</button>
     </div>
+    <!-- {{ state }} -->
+
     <div class="" v-for="(transaction, index) in state.data.data" :key="index">
       {{ transaction }}
     </div>
@@ -12,12 +15,18 @@
 </template>
 
 <script setup>
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { reactive, onMounted, watch } from "vue";
 import { useStore } from "vuex";
+import { deleteInvestment } from "@/firebase/database.js";
+
+const router = useRouter();
+// const route = useRoute();
+
 const store = useStore();
 const state = reactive({
   name: "",
+  docID: "",
   data: null,
 });
 onMounted(() => {
@@ -37,8 +46,25 @@ watch(
 );
 const getCrypto = async () => {
   const storeState = await store.state.cryptos.userCryptos;
+  const docIDS = await store.state.cryptos.userCryptosID;
   let data = storeState.find((crypto) => crypto.collection === state.name);
+  let findDocID = docIDS.find((id) => id.collection === state.name);
+  console.log(findDocID);
+  state.docID = findDocID.docID;
   state.data = { ...data };
+};
+
+const deleteDocument = () => {
+  if (
+    confirm(
+      "Are you sure you want to delete this WHOLE investment? Cannot be undone."
+    )
+  ) {
+    deleteInvestment(state.docID);
+    router.push({
+      path: "/",
+    });
+  }
 };
 </script>
 
