d697ad12b19160fa452513eb12bf8502402e5e62
Eric Wong <ericwong12@live.com>
2022-02-12 22:36:50 -0700

    Style: NewInvestment.vue
    Change input border

--- src/components/NewInvestment/NewInvestment.vue
+++ src/components/NewInvestment/NewInvestment.vue
@@ -1,6 +1,8 @@
 <template>
   <!-- Wait for User to input a crypto the want -->
-  <form class="flex justify-between items-center border-zinc-700 border-2 mb-4">
+  <form
+    class="flex justify-between items-center border-zinc-700 border-2 mb-4 rounded-md"
+  >
     <span class="z-10 absolute">
       <font-awesome-icon class="icon ml-1" icon="search" />
     </span>
@@ -9,8 +11,7 @@
       placeholder="Start search"
       v-model="investment.cryptoName"
       @input="filterData"
-      class="w-full outline-none pl-6 relative"
-      v-focus
+      class="w-full outline-none pl-6 relative rounded-md"
     />
   </form>
   <SearchResults
