6a81e8e3dd3587071cda7a445a2e6b89e113283a
Eric Wong <ericwong12@live.com>
2022-02-17 16:27:03 -0700

    Style: Navbar.vue
    Layout change.
    Buttons larger to fit space.
    Modal moved into file to remove one less file.

--- src/components/UI/Navigation/Navbar.vue
+++ src/components/UI/Navigation/Navbar.vue
@@ -1,7 +1,10 @@
 <template>
-  <nav class="bg-nav-bg flex flex-col items-center">
-    <router-link to="/"><div class="text-xl">Investment Log</div></router-link>
-    <div class="flex user justify-center items-center flex-auto">
+  <nav class="bg-nav-bg">
+    <router-link to="/">
+      <div class="text-xl text-center mt-10 mb-10">Investment Log</div>
+    </router-link>
+    <!-- Navigation buttons -->
+    <div class="grid grid-rows-6 gap-8 h-3/4">
       <!-- Sign in -->
       <Button
         v-if="!isLogin"
@@ -9,34 +12,41 @@
         :Icon="['fab', 'google']"
         name="Sign In"
       ></Button>
-
-      <!-- <div class="signIn" v-if="!isLogin">
-        <SignInModal :googleSignIn="signIn" />
-      </div> -->
-
-      <!-- Logged in -->
-      <div class="user" v-else>
-        <div class="avatar mr-4">
-          <!-- Future features -->
-          <Button name="My Account"></Button>
-        </div>
-        <UserInvestment />
-
-        <Button @runFunction="googleSignOut" name="Sign Out"></Button>
-      </div>
+      <!-- Logged in buttons -->
+      <Button v-if="isLogin" name="My Account" Icon="user-circle"></Button>
+      <Button
+        v-if="isLogin"
+        @runFunction="toggleModal"
+        Icon="plus"
+        name="New Investment"
+      ></Button>
+      <Button
+        v-if="isLogin"
+        Icon="arrow-alt-circle-right"
+        @runFunction="googleSignOut"
+        name="Sign Out"
+      ></Button>
+      <Modal @close="toggleModal" :modalActive="modalActive">
+        <NewInvestment />
+      </Modal>
     </div>
   </nav>
 </template>
 
 <script setup>
-import { useStore } from "vuex";
+import { ref } from "vue";
 import { useAuth } from "@/firebase/user.js";
 import SignInModal from "./Auth/SignIn.vue";
 import Register from "./Auth/Register.vue";
 import Button from "@/components/UI/Button.vue";
-import UserInvestment from "@/components/UserInvestment/index.vue";
+import NewInvestment from "@/components/NewInvestment/NewInvestment.vue";
+import Modal from "@/components/UI/Modal/index.vue";
 
-// const isLogin = false;
+const modalActive = ref(false);
+const toggleModal = () => {
+  modalActive.value = !modalActive.value;
+};
+
 const { user, isLogin, googleSignIn, googleSignOut } = useAuth();
 </script>
 
