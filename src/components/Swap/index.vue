0b855eac78d461145894d9f54cb4ae3c53f1792d
Eric Wong <ericwong12@live.com>
2022-02-28 16:50:12 -0700

    Feat: Swap.vue
    Add price swap return

--- src/components/Swap/index.vue
+++ src/components/Swap/index.vue
@@ -3,13 +3,14 @@
 -->
 
 <template>
-  <div class="border h-5/6 w-1/3 bg-slate-600 rounded-lg text-white">
+  <div class="border w-1/3 bg-slate-600 rounded-lg text-white">
     <div class="grid gap-12 p-5">
       <h2>Swap Assets</h2>
       <!-- From input -->
       <FromInput
         :userCrypto="userCrypto"
         :selectedCryptoOne="selectedCryptoOne"
+        @updateTradeQuantity="tradeQuantity"
         @updateCrypto="changeCryptoOne"
       />
 
@@ -24,7 +25,13 @@
         :selectedCryptoTwo="selectedCryptoTwo"
         @updateCrypto="changeCryptoTwo"
       />
-      <div class="border text-right">{{ swapPrice }}</div>
+      <div class="flex flex-col border text-right">
+        <div>{{ price.swapPrice }}</div>
+        <div v-if="price.tradeQuantity !== ''" class="">
+          <div>{{ price.tradePrice }}</div>
+          <div>USD amount: ${{ price.usdAmount }}</div>
+        </div>
+      </div>
       <Button name="Swap" />
     </div>
   </div>
@@ -45,13 +52,21 @@ const selectedCryptoOne = reactive({
   index: 0,
   symbol: null,
   current_price: null,
+  quantity: null,
 });
 const selectedCryptoTwo = reactive({
   index: 1,
   symbol: null,
   current_price: null,
+  quantity: null,
 });
-const swapPrice = ref(null);
+const price = reactive({
+  usdAmount: null,
+  swapPrice: null,
+  tradePrice: null,
+  quantity: null,
+  tradeQuantity: null,
+});
 const store = useStore();
 const userCrypto = store.getters["cryptos/getUserCrypto"];
 onMounted(() => {
@@ -64,33 +79,46 @@ onMounted(() => {
 const updateValue = () => {
   selectedCryptoOne.current_price = userCrypto[0].current_price;
   selectedCryptoOne.symbol = userCrypto[0].symbol;
+  selectedCryptoOne.quantity = userCrypto[0].total_quantity;
   selectedCryptoTwo.current_price = userCrypto[1].current_price;
   selectedCryptoTwo.symbol = userCrypto[1].symbol;
+  selectedCryptoTwo.quantity = userCrypto[1].total_quantity;
 };
 const calculatePrice = async () => {
   const priceOne = await selectedCryptoOne.current_price;
   const priceTwo = await selectedCryptoTwo.current_price;
   const symbolOne = await selectedCryptoOne.symbol;
   const symbolTwo = await selectedCryptoTwo.symbol;
-
-  console.log(priceOne, priceTwo);
-  // if (priceOne >= priceTwo) {
   let swapReturn = priceOne / priceTwo;
-  console.log(swapReturn);
-  swapPrice.value = `One ${symbolOne.toUpperCase()} = ${swapReturn} ${symbolTwo.toUpperCase()}`;
-  // }
+  price.swapPrice = `One ${symbolOne.toUpperCase()} = ${swapReturn} ${symbolTwo.toUpperCase()}`;
 };
 const changeCryptoOne = (data) => {
+  console.log(data);
   selectedCryptoOne.index = data.index;
   selectedCryptoOne.current_price = data.current_price;
   selectedCryptoOne.symbol = data.symbol;
+  selectedCryptoOne.quantity = data.quantity;
   calculatePrice();
 };
 const changeCryptoTwo = (data) => {
   selectedCryptoTwo.index = data.index;
   selectedCryptoTwo.current_price = data.current_price;
   selectedCryptoTwo.symbol = data.symbol;
+  selectedCryptoTwo.quantity = data.total_quantity;
+
   calculatePrice();
+};
+const tradeQuantity = async (data) => {
+  price.tradeQuantity = data.value;
+  const priceOne = await selectedCryptoOne.current_price;
+  const priceTwo = await selectedCryptoTwo.current_price;
+  const symbolOne = await selectedCryptoOne.symbol;
+  const symbolTwo = await selectedCryptoTwo.symbol;
+
+  let tradeReturn = (priceOne * data.value) / priceTwo;
+  price.tradePrice = `${price.tradeQuantity} 
+    ${symbolOne.toUpperCase()} = ${tradeReturn} ${symbolTwo.toUpperCase()}`;
+  price.usdAmount = priceOne * data.value;
 };
 </script>
 
