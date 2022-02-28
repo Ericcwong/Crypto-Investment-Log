b83c3241a21760b4516936b275af0d3a50da4bc3
Eric Wong <ericwong12@live.com>
2022-02-28 16:49:44 -0700

    Feat/Fix: Frominput.vue
    Add choice of 50% or max.
    Change crypto.
    
    Fix: errors

--- src/components/Swap/FromInput.vue
+++ src/components/Swap/FromInput.vue
@@ -1,27 +1,45 @@
 <template>
   <div class="input-container">
     <label for="asset-1 text-2xl ">From</label>
+
     <div class="flex border-2 p-4 rounded-lg">
-      <input class="w-full text-3xl bg-slate-600" type="" placeholder="0.00" />
-      <Modal @close="toggleModal" :modalActive="modalActive">
-        <CryptoList @changeCrypto="changeCryptoOne"
-      /></Modal>
-      <button @click="toggleModal">
-        <div class="flex items-center">
-          <img
-            class="w-7"
-            :src="userCrypto[selectedCryptoOne.index].icon"
-            alt="icon"
-          />
-          <div class="flex items-center pl-2 pr-3">
-            <span class="uppercase mr-1"
-              >{{ userCrypto[selectedCryptoOne.index].symbol }}
-            </span>
-            <font-awesome-icon class="text-sm" icon="caret-down" />
+      <input
+        class="w-full text-3xl bg-slate-600"
+        pattern="\d*"
+        placeholder="0.00"
+        v-model.number="tradeQuantity"
+        @input="checkAmount"
+      />
+      <div class="flex flex-col">
+        <button @click="toggleModal">
+          <div class="flex items-center">
+            <img
+              class="w-7"
+              :src="userCrypto[selectedCryptoOne.index].icon"
+              alt="icon"
+            />
+            <div class="flex items-center pl-2 pr-3">
+              <span class="uppercase mr-1"
+                >{{ userCrypto[selectedCryptoOne.index].symbol }}
+              </span>
+              <font-awesome-icon class="text-sm" icon="caret-down" />
+            </div>
           </div>
+        </button>
+
+        <div class="flex justify-between">
+          <button @click="half">50%</button>
+          <button @click="max">Max</button>
         </div>
-      </button>
+      </div>
     </div>
+    <span v-if="overQuantity" class="text-red-400"
+      >Amount exceeds over quantity amount!</span
+    >
+
+    <Modal @close="toggleModal" :modalActive="modalActive">
+      <CryptoList @changeCrypto="changeCryptoOne"
+    /></Modal>
   </div>
 </template>
 
@@ -34,14 +52,33 @@ const props = defineProps({
   userCrypto: Array,
   selectedCryptoOne: Object,
 });
-const emit = defineEmits(["updateCrypto"]);
+const emit = defineEmits(["updateCrypto", "updateTradeQuantity"]);
+
+const tradeQuantity = ref(null);
+const overQuantity = ref(false);
 const modalActive = ref(false);
+
 const toggleModal = () => {
   modalActive.value = !modalActive.value;
 };
 const changeCryptoOne = (data) => {
-  console.log(data);
   emit("updateCrypto", data);
   toggleModal();
+};
+const half = () => {
+  tradeQuantity.value = props.selectedCryptoOne.quantity / 2;
+};
+const max = () => {
+  tradeQuantity.value = props.selectedCryptoOne.quantity;
+};
+const checkAmount = () => {
+  if (tradeQuantity.value <= props.selectedCryptoOne.quantity) {
+    emit("updateTradeQuantity", tradeQuantity);
+    overQuantity.value = false;
+
+    return;
+  } else {
+    overQuantity.value = true;
+  }
 };
 </script>
