1fd40102f982902be8cf64cb56da9f36aea5230f
Eric Wong <ericwong12@live.com>
2022-02-28 16:50:59 -0700

    Feat: Homepage.vue
    Displays information about project when user first comes to the website.

---
+++ src/components/UI/HomePage/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>
+      Crypto investment log or Moon Rocket is a mimic of a web 3's decentralized
+      exchange's swap feature.
+    </h1>
+  </div>
+</template>
+
+<script setup></script>
