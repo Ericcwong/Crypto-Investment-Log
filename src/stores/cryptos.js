38237324981e27814b3a8ea2e77127be0912edd6
Eric Wong <ericwong12@live.com>
2022-03-03 16:45:16 -0700

    Feat: cryptos.js (stores)
    Pinia store

---
+++ src/stores/cryptos.js
@@ -0,0 +1,130 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+export const useCryptoStore = defineStore("crypto", {
+  state: () => {
+    return {
+      cryptos: [],
+      userCryptos: [],
+      userCryptosID: [],
+    };
+  },
+  actions: {
+    loadCryptos(payload) {
+      payload.forEach((element) => {
+        this.cryptos.push(element);
+      });
+    },
+    loadUserCrypto(payload) {
+      this.userCryptos = [];
+      payload.forEach((element) => {
+        this.userCryptos.push(element);
+      });
+    },
+    updateUserCrypto(payload) {
+      payload.forEach((element) => {
+        this.userCryptos.push(element);
+      });
+    },
+    loadUserCryptosID(payload) {
+      this.userCryptosID = payload;
+    },
+    clearState() {
+      this.cryptos = [];
+    },
+    clearUserCrypto() {
+      this.userCryptos = [];
+    },
+    async loadCryptos() {
+      try {
+        // Looping through 1-15 because Coingecko's API only allows 250 per page. Calling in 15 pages
+        for (let i = 1; i <= 2; i++) {
+          let response = await axios.get(
+            `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=${i}&sparkline=false`
+          );
+          console.log(response);
+          let cryptos = response.data;
+          cryptos.forEach((element) => {
+            this.cryptos.push(element);
+          });
+          //   this.loadCryptos(cryptos);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async loadUserCryptos(payload) {
+      try {
+        const data = await this.calculateUserCrypto(payload);
+        this.loadUserCrypto(data); // Need to pass calculated total afterwards
+      } catch (error) {
+        console.log("Loading user's crypto", error);
+      }
+    },
+    async calculateUserCrypto(payload) {
+      const calculatedPayload = Promise.all(
+        payload.map(async (doc) => {
+          let totalPrice = doc.data.reduce((previous, current) => {
+            if (current.transaction_type === "Buy") {
+              return previous + current.price * current.quantity;
+            } else {
+              return previous - current.price * current.quantity;
+            }
+          }, 0);
+          let totalQuantity = doc.data.reduce((previous, current) => {
+            if (current.transaction_type === "Buy") {
+              return previous + current.quantity;
+            } else {
+              return previous - current.quantity;
+            }
+            // return previous + current.quantity;
+          }, 0);
+          let price = await axios.get(
+            `https://api.coingecko.com/api/v3/simple/price?ids=${doc.collection}&vs_currencies=usd`
+          );
+          let currentPrice = price.data[doc.collection].usd;
+
+          // // Creates a new property within data array!
+          doc.current_price = currentPrice;
+          doc.total_price = totalPrice;
+          doc.total_quantity = totalQuantity;
+          if (totalQuantity > 0) {
+            doc.average_price = (totalPrice / totalQuantity).toFixed(2);
+          }
+          doc.profit_loss = (currentPrice * totalQuantity - totalPrice).toFixed(
+            2
+          );
+          // console.log(doc);
+          return doc;
+        })
+      );
+      return calculatedPayload;
+    },
+  },
+  getters: {
+    getCryptoByName: (state) => (name) => {
+      let data = state.cryptos.filter((crypto) => {
+        let regex = new RegExp(`(^${name})`, "gim");
+        return crypto.name.match(regex) || crypto.symbol.match(regex);
+      });
+      console.log(data);
+      return data;
+    },
+    getState: (state) => state.userCryptos,
+    // Filter array of duplicates and returns only one of each crypto
+    getUserCrypto(state) {
+      console.log(state.userCryptos);
+      let data = state.userCryptos.sort(async (a, b) => {
+        // Sorts and orders cryptos by highest total asset price.
+        return (await b.total_price) - (await a.total_price);
+      });
+      return data;
+    },
+    getCrypto: (state) => async (name) => {
+      let data = state.userCryptos.filter((crypto) => {
+        crypto.collection = name;
+      });
+      console.log(data);
+      return data;
+    },
+  },
+});
