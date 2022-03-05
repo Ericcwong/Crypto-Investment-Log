e479fecdff246e646466b0169a3629249148838f
Eric Wong <ericwong12@live.com>
2022-03-05 16:21:18 -0700

    Fix: Home.vue simplify file.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,36 +1,36 @@
 <template>
   <section
     id="current-investment"
-    class="grid grid-flow-col grid-cols-2 grid-rows-6 h-screen"
+    class="grid grid-flow-col grid-cols-2 grid-rows-6 min-h-screen bg-gray-900"
   >
     <HomeContent
       v-if="!isLogin"
       class="flex justify-center items-center col-span-2 row-span-6"
     />
     <!-- If user is logged in -->
-    <div
-      v-if="isLogin"
-      class="flex justify-center items-center col-span-2 row-span-4"
-    >
-      <Swap />
-    </div>
-
-    <div v-if="isLogin" class="col-span-2 row-span-2 bg-nav-bg">
-      <LoadInvestment />
-    </div>
+    <Dashboard v-if="isLogin" />
   </section>
 </template>
 
 <script setup>
-import { computed, watchEffect } from "vue";
-import { useStore } from "vuex";
-import { useAuth } from "@/firebase/user";
+import { onMounted, ref, provide } from "vue";
 import HomeContent from "@/components/UI/HomePage/index.vue";
-import MarketData from "@/components/MarketData/index.vue";
-import LoadInvestment from "../components/LoadInvestment/index.vue";
-import Swap from "@/components/Swap/index.vue";
+import Dashboard from "./Dashboard.vue";
+import { onAuthStateChanged, getAuth } from "firebase/auth";
 
-const { isLogin, user } = useAuth();
+const isLogin = ref(false);
+let auth;
+onMounted(() => {
+  auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      isLogin.value = true;
+    } else {
+      isLogin.value = false;
+    }
+  });
+  provide("isLogin", isLogin);
+});
 </script>
 
 <style scoped></style>
