0f0dd9b3033fe4814fab879b3429e3e454b31799
Eric Wong <ericwong12@live.com>
2022-03-05 16:18:16 -0700

    Fix: FromInput.vue
    Works but needs further tests.

--- src/components/Swap/FromInput.vue
+++ src/components/Swap/FromInput.vue
@@ -1,59 +1,63 @@
 <template>
-  <div class="input-container">
-    <label for="asset-1 text-2xl ">From</label>
-
-    <div class="flex border-2 p-4 rounded-lg">
-      <input
-        class="w-full text-3xl bg-slate-600"
-        pattern="\d*"
-        placeholder="0.00"
-        v-model.number="tradeQuantity"
-        @input="checkAmount"
-      />
-      <div class="flex flex-col">
-        <button @click="toggleModal">
-          <div class="flex items-center">
-            <img
-              class="w-7"
-              :src="userCrypto[selectedCryptoOne.index].icon"
-              alt="icon"
-            />
-            <div class="flex items-center pl-2 pr-3">
-              <span class="uppercase mr-1"
-                >{{ userCrypto[selectedCryptoOne.index].symbol }}
-              </span>
-              <font-awesome-icon class="text-sm" icon="caret-down" />
-            </div>
+  <label for="from">From</label>
+  <div class="flex border-2 p-4 rounded-lg">
+    <input
+      class="w-full text-3xl bg-slate-600"
+      placeholder="0.00"
+      mode="decimal"
+      v-model="tradeQuantity"
+      :minFractionDigits="2"
+      @input="checkAmount"
+    />
+    <div class="flex flex-col">
+      <button @click="toggleModal">
+        <div class="flex items-center">
+          <img
+            class="w-7"
+            v-if="chosenCrypto.crypto"
+            :src="chosenCrypto.crypto.icon"
+            alt="icon"
+          />
+          <div class="flex items-center pl-2 pr-3">
+            <span v-if="chosenCrypto.crypto" class="uppercase mr-1"
+              >{{ chosenCrypto.crypto.symbol }}
+            </span>
+            <font-awesome-icon class="text-sm" icon="caret-down" />
           </div>
-        </button>
-
-        <div class="flex justify-between">
-          <button @click="half">50%</button>
-          <button @click="max">Max</button>
         </div>
+      </button>
+
+      <div class="flex justify-between">
+        <button @click="half">50%</button>
+        <button @click="max">Max</button>
       </div>
     </div>
-    <span v-if="overQuantity" class="text-red-400"
-      >Amount exceeds over quantity amount!</span
-    >
-
-    <Modal @close="toggleModal" :modalActive="modalActive">
-      <CryptoList @changeCrypto="changeCryptoOne"
-    /></Modal>
   </div>
+  <span v-if="overQuantity" class="text-red-400"
+    >Amount exceeds over quantity amount!
+  </span>
+
+  <Modal @close="toggleModal" :modalActive="modalActive">
+    <CryptoList @changeCrypto="changeCrypto" />
+  </Modal>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, reactive, computed } from "vue";
+import InputNumber from "primevue/inputnumber";
 import Modal from "@/components/UI/Modal/index.vue";
 import Button from "@/components/UI/Button.vue";
 import CryptoList from "./CryptoList.vue";
-const props = defineProps({
-  userCrypto: Array,
-  selectedCryptoOne: Object,
-});
-const emit = defineEmits(["updateCrypto", "updateTradeQuantity"]);
+import { useCryptoStore } from "../../stores/cryptos";
+const cryptoStore = useCryptoStore();
 
+const emit = defineEmits(["updateCrypto", "updateTradeQuantity"]);
+console.log(cryptoStore.userCryptos);
+const chosenCrypto = reactive({
+  crypto: computed(() => {
+    return cryptoStore.userCryptos[0];
+  }),
+});
 const tradeQuantity = ref(null);
 const overQuantity = ref(false);
 const modalActive = ref(false);
@@ -61,22 +65,23 @@ const modalActive = ref(false);
 const toggleModal = () => {
   modalActive.value = !modalActive.value;
 };
-const changeCryptoOne = (data) => {
-  emit("updateCrypto", data);
+const changeCrypto = (data) => {
+  chosenCrypto.crypto = data;
   toggleModal();
 };
 const half = () => {
-  tradeQuantity.value = props.selectedCryptoOne.quantity / 2;
+  console.log(chosenCrypto.crypto);
+  console.log(tradeQuantity.value);
+  tradeQuantity.value = chosenCrypto.crypto.total_quantity / 2;
 };
 const max = () => {
-  tradeQuantity.value = props.selectedCryptoOne.quantity;
+  tradeQuantity.value = chosenCrypto.crypto.total_quantity;
 };
 const checkAmount = () => {
-  if (tradeQuantity.value <= props.selectedCryptoOne.quantity) {
-    emit("updateTradeQuantity", tradeQuantity);
+  console.log(tradeQuantity.value);
+  console.log(chosenCrypto.crypto.total_quantity);
+  if (tradeQuantity.value <= chosenCrypto.crypto.total_quantity) {
     overQuantity.value = false;
-
-    return;
   } else {
     overQuantity.value = true;
   }
