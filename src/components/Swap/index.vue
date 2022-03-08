e08958aaf4159400e9a6cf3b7d1b8adb4510bcc7
Eric Wong <ericwong12@live.com>
2022-03-08 16:35:45 -0700

    Feat: Swap.vue
    Check if user entered amount isn't over amount they have.
    If over, submit button doesn't work.

--- src/components/Swap/index.vue
+++ src/components/Swap/index.vue
@@ -1,18 +1,26 @@
 <template>
   <div
-    class="swap-container w-full md:w-1/2 lg:w-1/4 border bg-gray-800 text-white rounded grid gap-12 p-5"
+    class="swap-container w-full md:w-1/2 lg:w-1/3 border bg-gray-800 text-white rounded grid gap-12 p-5"
   >
     <h2>Swap</h2>
     <!-- From Input -->
-    <FromInput />
+    <FromInput @amountPass="checkAmount" />
     <!-- Switch from, to. -->
     <div class="flex justify-center items-center">
-      <img src="https://img.icons8.com/ios-glyphs/30/000000/change--v1.png" />
+      <img
+        @click.prevent="swapCrypto"
+        src="https://img.icons8.com/ios-glyphs/30/000000/change--v1.png"
+      />
     </div>
 
     <!-- To Input -->
     <ToInput />
-    <Button name="Submit" />
+    <Button
+      :class="
+        overAmount ? ' bg-red-400 text-decoration: line-through' : 'text-white'
+      "
+      name="Submit"
+    />
   </div>
 </template>
 
@@ -22,10 +30,12 @@ import { useCryptoStore } from "../../stores/cryptos";
 import FromInput from "./FromInput.vue";
 import ToInput from "./ToInput.vue";
 import Button from "@/components/UI/Button.vue";
-
 const cryptoStore = useCryptoStore();
-const userCrypto = computed(() => {
-  return cryptoStore.userCryptos;
-});
-console.log(userCrypto.value);
+const overAmount = ref(false);
+
+const checkAmount = (data) => {
+  overAmount.value = data;
+};
+
+const swapCrypto = () => cryptoStore.swapCrypto();
 </script>
