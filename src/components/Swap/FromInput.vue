b824b7476b2cc688f88b3af9b4a322443e5620b9
Eric Wong <ericwong12@live.com>
2022-03-08 16:34:06 -0700

    Fix: FromInput.vue
    Input is now a number.
    Layout change
    Swaping between crypto one and two works.

--- src/components/Swap/FromInput.vue
+++ src/components/Swap/FromInput.vue
@@ -1,49 +1,52 @@
 <template>
-  <label for="from">From</label>
-  <div class="flex border-2 p-4 rounded-lg">
-    <input
-      class="w-full text-3xl bg-slate-600"
-      placeholder="0.00"
-      mode="decimal"
-      v-model="tradeQuantity"
-      :minFractionDigits="2"
-      @input="checkAmount"
-    />
-    <div class="flex flex-col">
-      <button @click="toggleModal">
-        <div class="flex items-center">
-          <img
-            class="w-7"
-            v-if="chosenCrypto.crypto"
-            :src="chosenCrypto.crypto.icon"
-            alt="icon"
-          />
-          <div class="flex items-center pl-2 pr-3">
-            <span v-if="chosenCrypto.crypto" class="uppercase mr-1"
-              >{{ chosenCrypto.crypto.symbol }}
-            </span>
-            <font-awesome-icon class="text-sm" icon="caret-down" />
+  <div class="input-container">
+    <label for="from">From</label>
+    <div class="flex border-2 p-2 rounded-lg">
+      <input
+        class="w-4/5 text-3xl bg-inherit mr-2"
+        placeholder="0.00"
+        type="number"
+        mode="decimal"
+        v-model="tradeQuantity"
+        :minFractionDigits="2"
+        @input="checkAmount"
+      />
+      <div class="grid grid-cols-2 grid-rows-2 gap-4">
+        <button class="col-span-2" @click="toggleModal">
+          <div class="flex items-center justify-between">
+            <img
+              class="w-5"
+              v-if="chosenCrypto.crypto"
+              :src="chosenCrypto.crypto.icon"
+              alt="icon"
+            />
+            <div class="flex items-center justify-between">
+              <span v-if="chosenCrypto.crypto" class="uppercase mr-1"
+                >{{ chosenCrypto.crypto.symbol }}
+              </span>
+              <font-awesome-icon class="text-sm" icon="caret-down" />
+            </div>
           </div>
-        </div>
-      </button>
+        </button>
 
-      <div class="flex justify-between">
+        <!-- <div class="flex justify-between"> -->
         <button @click="half">50%</button>
         <button @click="max">Max</button>
+        <!-- </div> -->
       </div>
     </div>
-  </div>
-  <span v-if="overQuantity" class="text-red-400"
-    >Amount exceeds over quantity amount!
-  </span>
+    <span v-if="overQuantity" class="text-red-400"
+      >Amount exceeds over quantity amount!
+    </span>
 
-  <Modal @close="toggleModal" :modalActive="modalActive">
-    <CryptoList @changeCrypto="changeCrypto" />
-  </Modal>
+    <Modal @close="toggleModal" :modalActive="modalActive">
+      <CryptoList @changeCrypto="changeCrypto" />
+    </Modal>
+  </div>
 </template>
 
 <script setup>
-import { ref, reactive, computed } from "vue";
+import { ref, reactive, watchEffect } from "vue";
 import InputNumber from "primevue/inputnumber";
 import Modal from "@/components/UI/Modal/index.vue";
 import Button from "@/components/UI/Button.vue";
@@ -51,12 +54,16 @@ import CryptoList from "./CryptoList.vue";
 import { useCryptoStore } from "../../stores/cryptos";
 const cryptoStore = useCryptoStore();
 
-const emit = defineEmits(["updateCrypto", "updateTradeQuantity"]);
+const emit = defineEmits(["amountPass"]);
 console.log(cryptoStore.userCryptos);
 const chosenCrypto = reactive({
-  crypto: computed(() => {
-    return cryptoStore.userCryptos[0];
-  }),
+  crypto: null,
+});
+// Vue documentation for watchEffect: https://vuejs.org/guide/essentials/watchers.html#watcheffect
+// Watch effect runs instantly and constantly watches for changes!
+watchEffect(async () => {
+  const response = cryptoStore.fromCrypto;
+  chosenCrypto.crypto = response;
 });
 const tradeQuantity = ref(null);
 const overQuantity = ref(false);
@@ -67,6 +74,7 @@ const toggleModal = () => {
 };
 const changeCrypto = (data) => {
   chosenCrypto.crypto = data;
+  cryptoStore.fromCrypto = data;
   toggleModal();
 };
 const half = () => {
@@ -82,8 +90,10 @@ const checkAmount = () => {
   console.log(chosenCrypto.crypto.total_quantity);
   if (tradeQuantity.value <= chosenCrypto.crypto.total_quantity) {
     overQuantity.value = false;
+    emit("amountPass", overQuantity.value);
   } else {
     overQuantity.value = true;
+    emit("amountPass", overQuantity.value);
   }
 };
 </script>
