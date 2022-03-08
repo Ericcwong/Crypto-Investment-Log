26be69abc92cb482f87272edf9e770abe53d3ed7
Eric Wong <ericwong12@live.com>
2022-03-08 16:36:11 -0700

    Style: ToInput.vue
    Fix layout to match FromInput

--- src/components/Swap/ToInput.vue
+++ src/components/Swap/ToInput.vue
@@ -3,22 +3,22 @@
     <label for="asset-2">To</label>
     <div class="flex border-2 p-4 rounded-lg">
       <input
-        class="w-full text-3xl bg-slate-600"
+        class="w-4/5 text-3xl bg-inherit mr-2"
         type="number"
         placeholder="0.00"
         v-model="tradeQuantity"
       />
 
-      <div class="flex flex-col">
-        <button @click="toggleModal">
-          <div class="flex items-center">
+      <div class="grid grid-cols-2 grid-rows-2 gap-4">
+        <button class="col-span-2" @click="toggleModal">
+          <div class="flex items-center justify-between">
             <img
+              class="w-5"
               v-if="chosenCrypto.crypto"
-              class="w-7"
               :src="chosenCrypto.crypto.icon"
               alt="icon"
             />
-            <div class="flex items-center ml-2 mr-3">
+            <div class="flex items-center justify-between">
               <span v-if="chosenCrypto.crypto" class="uppercase mr-1"
                 >{{ chosenCrypto.crypto.symbol }}
               </span>
@@ -26,10 +26,11 @@
             </div>
           </div>
         </button>
-        <div class="flex justify-between">
-          <button @click="half">50%</button>
-          <button @click="max">Max</button>
-        </div>
+
+        <!-- <div class="flex justify-between"> -->
+        <button @click="half">50%</button>
+        <button @click="max">Max</button>
+        <!-- </div> -->
       </div>
     </div>
     <Modal @close="toggleModal" :modalActive="modalActive">
@@ -39,7 +40,7 @@
 </template>
 
 <script setup>
-import { ref, reactive, computed } from "vue";
+import { ref, reactive, watchEffect } from "vue";
 import InputNumber from "primevue/inputnumber";
 import Modal from "@/components/UI/Modal/index.vue";
 import Button from "@/components/UI/Button.vue";
@@ -51,9 +52,13 @@ const props = defineProps({
 });
 
 const chosenCrypto = reactive({
-  crypto: computed(() => {
-    return cryptoStore.userCryptos[1];
-  }),
+  crypto: null,
+});
+// Vue documentation for watchEffect: https://vuejs.org/guide/essentials/watchers.html#watcheffect
+// Watch effect runs instantly and constantly watches for changes!
+watchEffect(async () => {
+  const response = cryptoStore.toCrypto;
+  chosenCrypto.crypto = response;
 });
 
 const modalActive = ref(false);
@@ -63,6 +68,7 @@ const toggleModal = () => {
 };
 const changeCrypto = (data) => {
   chosenCrypto.crypto = data;
+  cryptoStore.toCrypto = data;
   toggleModal();
 };
 </script>
