e86e1e07fea444c4062af3f36d0535501f9fe975
Eric Wong <ericwong12@live.com>
2022-03-18 01:30:47 -0600

    Fix: Navbar.vue
    Navigation is changed to top nav bar
    Needs further testing and fixing.

--- src/components/UI/Navigation/Navbar.vue
+++ src/components/UI/Navigation/Navbar.vue
@@ -1,252 +1,172 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <div>
-    <!-- Mobile Sidebar -->
-    <TransitionRoot as="template" :show="sidebarOpen">
-      <Dialog
-        as="div"
-        class="fixed inset-0 flex z-40 md:hidden"
-        @close="sidebarOpen = false"
-      >
-        <TransitionChild
-          as="template"
-          enter="transition-opacity ease-linear duration-300"
-          enter-from="opacity-0"
-          enter-to="opacity-100"
-          leave="transition-opacity ease-linear duration-300"
-          leave-from="opacity-100"
-          leave-to="opacity-0"
-        >
-          <DialogOverlay class="fixed inset-0 bg-gray-600 bg-opacity-75" />
-        </TransitionChild>
-        <TransitionChild
-          as="template"
-          enter="transition ease-in-out duration-300 transform"
-          enter-from="-translate-x-full"
-          enter-to="translate-x-0"
-          leave="transition ease-in-out duration-300 transform"
-          leave-from="translate-x-0"
-          leave-to="-translate-x-full"
-        >
-          <div
-            class="relative flex-1 flex flex-col max-w-xs w-full bg-gray-800"
-          >
-            <TransitionChild
-              as="template"
-              enter="ease-in-out duration-300"
-              enter-from="opacity-0"
-              enter-to="opacity-100"
-              leave="ease-in-out duration-300"
-              leave-from="opacity-100"
-              leave-to="opacity-0"
+  <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex justify-between h-16">
+        <div class="flex">
+          <div class="-ml-2 mr-2 flex items-center md:hidden">
+            <!-- Mobile menu button -->
+            <DisclosureButton
+              class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
             >
-              <div class="absolute top-0 right-0 -mr-12 pt-2">
-                <button
-                  type="button"
-                  class="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                  @click="sidebarOpen = false"
+              <span class="sr-only">Open main menu</span>
+              <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+              <XIcon v-else class="block h-6 w-6" aria-hidden="true" />
+            </DisclosureButton>
+          </div>
+          <div class="flex-shrink-0 flex items-center">
+            <img
+              class="block lg:hidden h-8 w-auto"
+              src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+              alt="Workflow"
+            />
+            <img
+              class="hidden lg:block h-8 w-auto"
+              src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+              alt="Workflow"
+            />
+          </div>
+          <div class="hidden md:ml-6 md:flex md:items-center md:space-x-4">
+            <a
+              v-if="isLogin"
+              v-for="item in loginNav"
+              :key="item.name"
+              :href="item.href"
+              :class="[
+                item.current
+                  ? 'bg-gray-900 text-white'
+                  : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                'px-3 py-2 rounded-md text-sm font-medium',
+              ]"
+              :aria-current="item.current ? 'page' : undefined"
+              >{{ item.name }}</a
+            >
+          </div>
+        </div>
+        <button v-if="!isLogin" class="text-white" @click="googleSignIn()">
+          Sign In
+        </button>
+        <div class="flex items-center" v-if="isLogin">
+          <div class="flex-shrink-0">
+            <button
+              type="button"
+              class="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500"
+              @click.prevent="toggleModal"
+            >
+              <PlusSmIcon class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+              <span>New Investment</span>
+            </button>
+          </div>
+          <div class="hidden md:ml-4 md:flex-shrink-0 md:flex md:items-center">
+            <button
+              type="button"
+              class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+            >
+              <span class="sr-only">View notifications</span>
+              <BellIcon class="h-6 w-6" aria-hidden="true" />
+            </button>
+
+            <!-- Profile dropdown -->
+            <Menu as="div" class="ml-3 relative">
+              <div>
+                <MenuButton
+                  class="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                 >
-                  <span class="sr-only">Close sidebar</span>
-                  <XIcon class="h-6 w-6 text-white" aria-hidden="true" />
-                </button>
+                  <span class="sr-only">Open user menu</span>
+                  <img
+                    v-if="user"
+                    class="h-8 w-8 rounded-full"
+                    :src="user.photoURL"
+                    alt=""
+                  />
+                </MenuButton>
               </div>
-            </TransitionChild>
-            <div class="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
-              <div class="flex-shrink-0 flex items-center px-4 text-white">
-                <img class="h-8 w-auto" src="./rocket.svg" alt="Moon Rocket" />
-                <span>Moon Rocket</span>
-              </div>
-              <nav class="mt-5 flex-1 px-2 space-y-1">
-                <div v-if="!isLogin">
-                  <button
-                    v-for="item in defaultNav"
+              <transition
+                enter-active-class="transition ease-out duration-200"
+                enter-from-class="transform opacity-0 scale-95"
+                enter-to-class="transform opacity-100 scale-100"
+                leave-active-class="transition ease-in duration-75"
+                leave-from-class="transform opacity-100 scale-100"
+                leave-to-class="transform opacity-0 scale-95"
+              >
+                <MenuItems
+                  class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                >
+                  <MenuItem
+                    v-for="item in userNavigation"
                     :key="item.name"
-                    @click.prevent="item.runFunction"
-                    :class="[
-                      item.current
-                        ? 'bg-gray-900 text-white'
-                        : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                      'group flex items-center px-2 py-2 text-md font-medium rounded-md w-full',
-                    ]"
+                    v-slot="{ active }"
                   >
-                    <component
-                      :is="item.icon"
+                    <button
+                      @click.prevent="item.runFunction"
                       :class="[
-                        item.current
-                          ? 'text-gray-300'
-                          : 'text-gray-400 group-hover:text-gray-300',
-                        'mr-3 flex-shrink-0 h-6 w-6',
+                        active ? 'bg-gray-100' : '',
+                        'block px-4 py-2 text-sm text-gray-700',
                       ]"
-                      aria-hidden="true"
-                    />
-                    {{ item.name }}
-                  </button>
-                </div>
-                <div v-if="isLogin">
-                  <button
-                    v-for="item in loginNav"
-                    :key="item.name"
-                    @click.prevent="item.runFunction"
-                    :class="[
-                      item.current
-                        ? 'bg-gray-900 text-white'
-                        : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                      'group flex items-center px-2 py-2 text-md font-medium rounded-md w-full',
-                    ]"
-                  >
-                    <component
-                      :is="item.icon"
-                      :class="[
-                        item.current
-                          ? 'text-gray-300'
-                          : 'text-gray-400 group-hover:text-gray-300',
-                        'mr-3 flex-shrink-0 h-6 w-6',
-                      ]"
-                      aria-hidden="true"
-                    />
-                    {{ item.name }}
-                  </button>
-                </div>
-              </nav>
-            </div>
-            <div v-if="isLogin" class="flex-shrink-0 flex bg-gray-700 p-4">
-              <a href="#" class="flex-shrink-0 w-full group block">
-                <div class="flex items-center">
-                  <div>
-                    <img
-                      class="inline-block h-9 w-9 rounded-full"
-                      :src="user.photoURL"
-                      alt=""
-                    />
-                  </div>
-                  <div class="ml-3">
-                    <p class="text-sm font-medium text-white">
-                      {{ user.displayName }}
-                    </p>
-                    <p
-                      class="text-xs font-medium text-gray-300 group-hover:text-gray-200"
                     >
-                      Welcome back!
-                    </p>
-                  </div>
-                </div>
-              </a>
+                      {{ item.name }}
+                    </button>
+                  </MenuItem>
+                </MenuItems>
+              </transition>
+            </Menu>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <DisclosurePanel class="md:hidden">
+      <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+        <DisclosureButton
+          v-for="item in loginNav"
+          :key="item.name"
+          as="a"
+          :href="item.href"
+          :class="[
+            item.current
+              ? 'bg-gray-900 text-white'
+              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+            'block px-3 py-2 rounded-md text-base font-medium',
+          ]"
+          :aria-current="item.current ? 'page' : undefined"
+          >{{ item.name }}</DisclosureButton
+        >
+      </div>
+      <div class="pt-4 pb-3 border-t border-gray-700">
+        <div class="flex items-center px-5 sm:px-6">
+          <div class="flex-shrink-0">
+            <img class="h-10 w-10 rounded-full" :src="user.imageUrl" alt="" />
+          </div>
+          <div class="ml-3">
+            <div class="text-base font-medium text-white">
+              {{ user.displayName }}
             </div>
           </div>
-        </TransitionChild>
-        <div class="flex-shrink-0 w-14">
-          <!-- Force sidebar to shrink to fit close icon -->
+          <button
+            type="button"
+            class="ml-auto flex-shrink-0 bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+          >
+            <span class="sr-only">View notifications</span>
+            <BellIcon class="h-6 w-6" aria-hidden="true" />
+          </button>
         </div>
-      </Dialog>
-    </TransitionRoot>
-    <!-- End of mobile sidebar -->
-    <!-- Static sidebar for desktop -->
-    <div class="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
-      <!-- Sidebar component, swap this element with another sidebar if you like -->
-      <div class="flex-1 flex flex-col min-h-0 bg-gray-800">
-        <div class="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
-          <router-link to="/" class="flex items-center flex-shrink-0 px-4">
-            <img src="./rocket.svg" class="w-1/3" alt="" />
-            <h1 class="text-white">Moon Rocket</h1>
-          </router-link>
-
-          <nav class="mt-5 flex-1 px-2 space-y-1">
-            <div v-if="!isLogin">
-              <button
-                v-for="item in defaultNav"
-                :key="item.name"
-                @click.prevent="item.runFunction"
-                :class="[
-                  item.current
-                    ? 'bg-gray-900 text-white'
-                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                  'group flex items-center px-2 py-2 text-md font-medium rounded-md w-full',
-                ]"
-              >
-                <component
-                  :is="item.icon"
-                  :class="[
-                    item.current
-                      ? 'text-gray-300'
-                      : 'text-gray-400 group-hover:text-gray-300',
-                    'mr-3 flex-shrink-0 h-6 w-6',
-                  ]"
-                  aria-hidden="true"
-                />
-                {{ item.name }}
-              </button>
-            </div>
-            <div v-if="isLogin">
-              <button
-                v-for="item in loginNav"
-                :key="item.name"
-                @click.prevent="item.runFunction"
-                :class="[
-                  item.current
-                    ? 'bg-gray-900 text-white'
-                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                  'group flex items-center px-2 py-2 text-md font-medium rounded-md w-full',
-                ]"
-              >
-                <component
-                  :is="item.icon"
-                  :class="[
-                    item.current
-                      ? 'text-gray-300'
-                      : 'text-gray-400 group-hover:text-gray-300',
-                    'mr-3 flex-shrink-0 h-6 w-6',
-                  ]"
-                  aria-hidden="true"
-                />
-                {{ item.name }}
-              </button>
-            </div>
-          </nav>
-        </div>
-        <!-- Button User account -->
-        <div v-if="isLogin" class="flex-shrink-0 flex bg-gray-700 p-4">
-          <a href="#" class="flex-shrink-0 w-full group block">
-            <div class="flex items-center">
-              <div>
-                <img
-                  class="inline-block h-9 w-9 rounded-full"
-                  :src="user.photoURL"
-                  alt=""
-                />
-              </div>
-              <div class="ml-3">
-                <p class="text-sm font-medium text-white">
-                  {{ user.displayName }}
-                </p>
-                <p
-                  class="text-xs font-medium text-gray-300 group-hover:text-gray-200"
-                >
-                  Welcome back!
-                </p>
-              </div>
-            </div>
-          </a>
+        <div class="mt-3 px-2 space-y-1 sm:px-3">
+          <button
+            v-for="item in userNavigation"
+            :key="item.name"
+            as="a"
+            @click="item.runFunction"
+            class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
+          >
+            {{ item.name }}
+          </button>
         </div>
       </div>
-    </div>
-    <Modal @close="toggleModal" :modalActive="modalActive">
-      <NewInvestment />
-    </Modal>
-    <!-- Hamburger sidebar -->
-    <div class="md:pl-64 flex flex-col flex-1">
-      <div
-        class="sticky top-0 z-10 md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3 bg-gray-800"
-      >
-        <button
-          type="button"
-          class="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-300 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-          @click="sidebarOpen = true"
-        >
-          <span class="sr-only">Open sidebar</span>
-          <MenuIcon class="h-6 w-6" aria-hidden="true" />
-        </button>
-      </div>
-    </div>
-  </div>
+    </DisclosurePanel>
+  </Disclosure>
+  <Modal @close="toggleModal" :modalActive="modalActive">
+    <NewInvestment />
+  </Modal>
 </template>
 
 <script setup>
@@ -260,22 +180,26 @@ import Modal from "@/components/UI/Modal/index.vue";
 import { useRouter } from "vue-router";
 import { useUserStore } from "../../../stores/user";
 import {
-  Dialog,
-  DialogOverlay,
-  TransitionChild,
-  TransitionRoot,
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
 } from "@headlessui/vue";
 import {
-  CalendarIcon,
-  ChartBarIcon,
-  FolderIcon,
-  HomeIcon,
-  InboxIcon,
+  BellIcon,
   MenuIcon,
-  UsersIcon,
   XIcon,
+  HomeIcon,
+  PlusIcon,
+  LogoutIcon,
+  LoginIcon,
 } from "@heroicons/vue/outline";
-import { LogoutIcon, PlusIcon, LoginIcon } from "@heroicons/vue/solid";
+
+import { PlusSmIcon } from "@heroicons/vue/solid";
+
 const userStore = useUserStore();
 const router = useRouter();
 const modalActive = ref(false);
@@ -301,22 +225,6 @@ const loginNav = [
     icon: HomeIcon,
     current: true,
   },
-  {
-    name: "New Investment",
-    runFunction: () => {
-      toggleModal();
-    },
-    icon: PlusIcon,
-    current: false,
-  },
-  {
-    name: "Sign Out",
-    runFunction: () => {
-      googleSignOut();
-    },
-    icon: LogoutIcon,
-    current: false,
-  },
 ];
 const defaultNav = [
   {
@@ -328,5 +236,17 @@ const defaultNav = [
     current: true,
   },
 ];
+const userNavigation = [
+  {
+    name: "Sign Out",
+    runFunction: () => {
+      console.log("Clicked");
+      googleSignOut();
+    },
+    icon: LogoutIcon,
+    current: false,
+  },
+];
 const { user, isLogin, googleSignIn, googleSignOut } = useAuth();
+console.log(user);
 </script>
